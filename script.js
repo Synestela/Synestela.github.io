61b2edef722708d82f8914c822dc3bb18c72bb22
Synestela <umutkabakci029@gmail.com>
2025-06-29 14:45:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 btn.classList.remove('selected');
             });
             // Add 'selected' class to the clicked button
-            .classList.add('selected');
+            button.classList.add('selected');
 
             // Hide any displayed messages when the user makes a new selection
             errorMessage.style.display = 'none';
